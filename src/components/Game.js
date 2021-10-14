c884d64399ea4511aec17a94edbdf718fad16297
Hyperion <radoslaw.lewandowski10@gmail.com>
2021-10-14 12:05:04 +0200

    Create handleClick

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,5 +6,14 @@ const Game = () => {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
-  const winner = calculateWinner(history[setStepNumber]);
+  const winner = calculateWinner(history[stepNumber]);
+  const x0 = xIsNext ? "X" : "0";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // return if won or occupied
+    if (winner || squares[i]) return;
+  };
 };
